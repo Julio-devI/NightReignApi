f96318a9a3ee1229c44f790642b51e2b73b1b738
julio <julioakzzo@hotmail.com>
2021-10-20 17:36:40 -0300

    correction the controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,17 +7,31 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return Product::all();
     }
 
-    /** 
-     * Store a newly created resource in storage. 
-     * 
-     * @param \Illuminate\Http\Request  $request
-     * @param \Illuminate\Http\Response
-     * 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -31,9 +45,9 @@ class ProductController extends Controller
     }
 
     /**
-     * Display the specified resource
-     * 
-     * @param int $id
+     * Display the specified resource.
+     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -41,17 +55,38 @@ class ProductController extends Controller
         return Product::find($id);
     }
 
-    /** 
-     * Update the specified resource in storage
-     * 
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        $product = Product::find($id);
+        $product = Product::findOrFail($id);
         $product->update($request->all());
         return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
