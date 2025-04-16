38da6ba36db5606a9440d381c438749b60397b24
julio-DevI <julio.a.valenca@gmail.com>
2025-04-16 20:03:16 -0300

    update: update laravel 11 for laravel 12

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class ItemController extends Controller
 {
     /**
      * Display a listing of the resource.
