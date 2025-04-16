9d50f64ce16f18a7ce141db86ccaf0a62465814f
julio-DevI <julio.a.valenca@gmail.com>
2025-04-16 19:37:46 -0300

    feat: make weapon categories table and add the foreign key in items table

---
+++ database/migrations/2025_04_16_223222_add_weapon_category_id_to_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->foreignId('weapon_category_id')
+                ->nullable()
+                ->constrained('weapon_categories')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropForeign(['weapon_category_id']);
+            $table->dropColumn('weapon_category_id');
+        });
+    }
+};
